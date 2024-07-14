826238db46684ccc2b7f2c8f7e85415ea03a690a
Saviour1001 <harshghodkar@gmail.com>
2024-07-14 19:57:02 +0530

    added drift template

--- src/templates.js
+++ src/templates.js
@@ -6,20 +6,26 @@ const templates = [
     path: "examples/axum",
   },
   {
-    name: "Express",
+    name: "Express by Solana Developers",
     description: "Express template for Solana Blinks by Solana Developers",
     repo: "https://github.com/solana-developers/solana-actions",
     path: "examples/express",
   },
   {
-    name: "NextJS Basic",
+    name: "Express by Drift Labs",
+    description: "Express template for Solana Blinks by Drift Labs",
+    repo: "https://github.com/drift-labs/solana-actions-server",
+    path: "",
+  },
+  {
+    name: "NextJS by Solana Developers",
     description:
       "Basic NextJS template for Solana Blinks from Solana Developers",
     repo: "https://github.com/solana-developers/solana-actions",
     path: "examples/next-js",
   },
   {
-    name: "NextJS",
+    name: "NextJS by Arihant",
     description: "NextJS template for Solana Blinks by Arihant",
     repo: "https://github.com/arihantbansal/solana-actions-template",
     path: "",
