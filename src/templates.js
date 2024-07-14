405ff50aa6125d65582588c31c6d646dea30d39c
Saviour1001 <harshghodkar@gmail.com>
2024-07-14 18:48:55 +0530

    Added Axum, Express and another Next Template

--- src/templates.js
+++ src/templates.js
@@ -1,17 +1,30 @@
 const templates = [
   {
+    name: "Axum",
+    description: "Axum template for Solana Blinks by Solana Developers",
+    repo: "https://github.com/solana-developers/solana-actions",
+    path: "examples/axum",
+  },
+  {
+    name: "Express",
+    description: "Express template for Solana Blinks by Solana Developers",
+    repo: "https://github.com/solana-developers/solana-actions",
+    path: "examples/express",
+  },
+  {
+    name: "NextJS Basic",
+    description:
+      "Basic NextJS template for Solana Blinks from Solana Developers",
+    repo: "https://github.com/solana-developers/solana-actions",
+    path: "examples/next-js",
+  },
+  {
     name: "NextJS",
     description: "NextJS template for Solana Blinks by Arihant",
     repo: "https://github.com/arihantbansal/solana-actions-template",
     path: "",
   },
-  {
-    name: "NextJS Basic",
-    description:
-      "Basic NextJS template for Solana Blinks from Solana Foundation",
-    repo: "https://github.com/solana-developers/solana-actions",
-    path: "examples/next-js",
-  },
+
   // Add more templates here
 ];
 
